2654348aba186f7cf3814e6aceed4c398f9bf6d9
Tatiana <tatianamoronta@ac.c4q.nyc>
2018-03-03 14:05:27 -0500

    added retrofit

--- app/src/main/java/com/example/murodjonrahimov/hackathon/MainActivity.java
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.murodjonrahimov.hackathon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://data.cityofnewyork.us/")
+            .build();
+
   }
 }
--- app/src/main/java/com/example/murodjonrahimov/hackathon/network/Service.java
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/network/Service.java
@@ -1,12 +1,21 @@
 package com.example.murodjonrahimov.hackathon.network;
 
+import com.example.murodjonrahimov.hackathon.model.Jobs;
+
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+
 /**
  * Created by c4q on 3/3/18.
  */
 
-public class Service {
+public interface Service{
 
 
+
+//Service call for Jobs JSON
+@GET("/resource/6fic-ympf.json")
+    Class<Jobs> getJobs();
 
 
 
