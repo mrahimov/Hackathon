c1be3266343b2df7b650160bb9478528c0cc0d34
Volha Kaleda <volhakaleda@ac.c4q.nyc>
2018-03-03 17:58:30 -0500

    bottombar navigation is working

---
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/TabsPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.murodjonrahimov.hackathon;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.murodjonrahimov.hackathon.fragments.ParksFragment;
+import com.example.murodjonrahimov.hackathon.fragments.PoolsFragment;
+
+/**
+ * Created by olgakoleda on 3/3/18.
+ */
+
+public class TabsPagerAdapter extends FragmentPagerAdapter{
+
+    private int NUMBER_OF_ITEMS = 3;
+    private String [] titles = new String[] {"Courts", "Pools", "Parks"};
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+
+            case 0:
+                return new PoolsFragment();
+            case 1:
+                return new ParksFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return NUMBER_OF_ITEMS;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
---
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/FavoriteFragment.java
@@ -0,0 +1,38 @@
+package com.example.murodjonrahimov.hackathon.fragments;
+
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.murodjonrahimov.hackathon.R;
+import com.example.murodjonrahimov.hackathon.TabsPagerAdapter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FavoriteFragment extends Fragment {
+
+
+    private ViewPager pager;
+    private TabsPagerAdapter adapter;
+
+    public FavoriteFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_favorite, container, false);
+return rootView;
+
+    }
+
+}
