931188fc290a84fda7611014926f3073635312e1
Volha Kaleda <volhakaleda@ac.c4q.nyc>
2018-03-04 00:01:56 -0500

    parks fragment complete

--- app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/ParksFragment.java
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/ParksFragment.java
@@ -1,17 +1,18 @@
 package com.example.murodjonrahimov.hackathon.fragments;
 
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.murodjonrahimov.hackathon.R;
-import com.example.murodjonrahimov.hackathon.model.IndoorPools;
+import com.example.murodjonrahimov.hackathon.controller.ParksAdapter;
 import com.example.murodjonrahimov.hackathon.model.Parks;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -20,19 +21,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-import static android.content.ContentValues.TAG;
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ParksFragment extends Fragment {
 
     private static final String PARK_RESPONSE = "response";
-
+    private RecyclerView recyclerView;
 
     public ParksFragment() {
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,12 +42,15 @@ public class ParksFragment extends Fragment {
 
         AsyncTaskRunner asyncTaskRunner = new AsyncTaskRunner();
         asyncTaskRunner.execute();
+
+        recyclerView = getActivity().findViewById(R.id.parks_recyclerview);
+
     }
 
     public String loadJSONFromAsset() {
         String json;
 
-        try{
+        try {
             InputStream is = getContext().getAssets().open("parks.json");
 
             int size = is.available();
@@ -60,8 +58,7 @@ public class ParksFragment extends Fragment {
             is.read(buffer);
             is.close();
             json = new String(buffer, "UTF-8");
-        }
-        catch (IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
@@ -70,23 +67,26 @@ public class ParksFragment extends Fragment {
 
     private class AsyncTaskRunner extends AsyncTask<String, String, ArrayList<Parks>> {
 
-
         @Override
         protected ArrayList<Parks> doInBackground(String... params) {
 
             String json = loadJSONFromAsset();
             Gson gson = new Gson();
-            ArrayList<Parks> listOfParks = gson.fromJson(json, new TypeToken<ArrayList<Parks>>() {}.getType());
+            ArrayList<Parks> listOfParks = gson.fromJson(json, new TypeToken<ArrayList<Parks>>() {
+            }.getType());
             return listOfParks;
         }
 
         @Override
-        protected void onPostExecute(ArrayList<Parks> listOfParks ) {
+        protected void onPostExecute(ArrayList<Parks> listOfParks) {
 
-            Parks park = listOfParks.get(2);
-            Log.d(PARK_RESPONSE, park.getName());
+//            Parks park = listOfParks.get(2);
+//            Log.d(PARK_RESPONSE, park.getName());
+
+            ParksAdapter parksAdapter = new ParksAdapter(listOfParks);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+            recyclerView.setAdapter(parksAdapter);
+            recyclerView.setLayoutManager(linearLayoutManager);
         }
-
     }
-
 }
