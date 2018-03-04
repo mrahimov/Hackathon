9fa0d5aa85df00244607e62c14ae9d7d26f3189a
Volha Kaleda <volhakaleda@ac.c4q.nyc>
2018-03-03 22:42:23 -0500

    parks and indoor pools data pulled from json, models created

--- app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/CourtsFragment.java
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/CourtsFragment.java
@@ -37,57 +37,57 @@ public class CourtsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        try {
-            getBasketBallJSON();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            getBasketBallJSON();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return inflater.inflate(R.layout.fragment_courts, container, false);
     }
 
-    public void getBasketBallJSON() throws IOException {
-        InputStream inputStream = getContext().getAssets().open("basketball_courts.json");
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        int size;
-        try{
-            size = inputStream.read();
-            while(size != -1) {
-                byteArrayOutputStream.write(size);
-                size = inputStream.read();
-            }
-            inputStream.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            JSONArray keyArray = new JSONArray(byteArrayOutputStream.toString());
-            Log.d("key array", keyArray.toString());
-            String name = "";
-            String location = "";
-            String numOfCourts = "";
-
-            for (int i = 0; i < keyArray.length(); i++) {
-                name = keyArray.getJSONObject(i).getString("Name");
-                location = keyArray.getJSONObject(i).getString("Location");
-                numOfCourts = keyArray.getJSONObject(i).getString("Num_of_Courts");
-
-                BasketBallCourt bc = new BasketBallCourt();
-                bc.setName(name);
-                bc.setLocation(location);
-                bc.setNum_of_Courts(numOfCourts);
-
-                basketBallCourts.add(bc);
-                Log.d("basketball name: ", bc.getName());
-                Log.d("basketball location: ", bc.getLocation());
-                Log.d("basketball courts: ", bc.getNum_of_Courts());
-
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
+//    public void getBasketBallJSON() throws IOException {
+//        InputStream inputStream = getContext().getAssets().open("basketball_courts.json");
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//
+//        int size;
+//        try{
+//            size = inputStream.read();
+//            while(size != -1) {
+//                byteArrayOutputStream.write(size);
+//                size = inputStream.read();
+//            }
+//            inputStream.close();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            JSONArray keyArray = new JSONArray(byteArrayOutputStream.toString());
+//            Log.d("key array", keyArray.toString());
+//            String name = "";
+//            String location = "";
+//            String numOfCourts = "";
+//
+//            for (int i = 0; i < keyArray.length(); i++) {
+//                name = keyArray.getJSONObject(i).getString("Name");
+//                location = keyArray.getJSONObject(i).getString("Location");
+//                numOfCourts = keyArray.getJSONObject(i).getString("Num_of_Courts");
+//
+//                BasketBallCourt bc = new BasketBallCourt();
+//                bc.setName(name);
+//                bc.setLocation(location);
+//                bc.setNum_of_Courts(numOfCourts);
+//
+//                basketBallCourts.add(bc);
+//                Log.d("basketball name: ", bc.getName());
+//                Log.d("basketball location: ", bc.getLocation());
+//                Log.d("basketball courts: ", bc.getNum_of_Courts());
+//
+//            }
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
