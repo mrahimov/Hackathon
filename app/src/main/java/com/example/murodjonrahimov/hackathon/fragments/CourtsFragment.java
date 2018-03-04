88cea47659275c898c5f2c2b0d6679e0421c77a2
Tatiana <tatianamoronta@ac.c4q.nyc>
2018-03-03 21:15:58 -0500

    jobs frag call

--- app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/CourtsFragment.java
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/CourtsFragment.java
@@ -14,6 +14,8 @@ import com.example.murodjonrahimov.hackathon.R;
  */
 public class CourtsFragment extends Fragment {
 
+    View rootView;
+
 
     public CourtsFragment() {
         // Required empty public constructor
@@ -23,8 +25,10 @@ public class CourtsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_courts, container, false);
+        rootView = inflater.inflate(R.layout.fragment_courts, container, false);
+
+
+        return rootView;
     }
 
 }
