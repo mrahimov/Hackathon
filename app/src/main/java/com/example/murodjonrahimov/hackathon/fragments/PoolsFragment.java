2d5332d90f5d72cb9e1a14074510bbb3d88c5cd8
Volha Kaleda <volhakaleda@ac.c4q.nyc>
2018-03-04 00:27:58 -0500

    parks and pools fully complete

--- app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/PoolsFragment.java
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/PoolsFragment.java
@@ -1,119 +1,103 @@
 package com.example.murodjonrahimov.hackathon.fragments;
 
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.murodjonrahimov.hackathon.R;
-import com.example.murodjonrahimov.hackathon.model.IndoorPools;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-public class PoolsFragment extends Fragment {
-
-    private static final String TAG = "response";
-=======
 import com.example.murodjonrahimov.hackathon.container.PoolsAdapter;
 import com.example.murodjonrahimov.hackathon.model.ModelPools;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
+
 import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class PoolsFragment extends Fragment {
 
-  View rootView;
-  List<ModelPools> poolsList;
+    View rootView;
+    List<ModelPools> poolsList;
 
-  public PoolsFragment() {
-    // Required empty public constructor
-  }
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    rootView = inflater.inflate(R.layout.fragment_pools, container, false);
-
-    RecyclerView poolsRecyclerView = rootView.findViewById(R.id.recyclerview_pool);
-    poolsList = new ArrayList<>();
-
-    try {
-      getPoolsJSON();
-    } catch (IOException e) {
-      e.printStackTrace();
+    public PoolsFragment() {
     }
 
-    PoolsAdapter adapter = new PoolsAdapter(poolsList);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+      rootView = inflater.inflate(R.layout.fragment_pools, container, false);
 
-    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
-    poolsRecyclerView.setAdapter(adapter);
-    poolsRecyclerView.setLayoutManager(linearLayoutManager);
+      RecyclerView poolsRecyclerView = rootView.findViewById(R.id.recyclerview_pool);
+      poolsList = new ArrayList<>();
 
-    return rootView;
-  }
+      try {
+        getPoolsJSON();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
 
-  public void getPoolsJSON() throws IOException {
-    InputStream inputStream = getContext().getAssets()
-      .open("pools_courts.json");
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      PoolsAdapter adapter = new PoolsAdapter(poolsList);
 
-    int size;
-    try {
-      size = inputStream.read();
-      while (size != -1) {
-        byteArrayOutputStream.write(size);
+      LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+      poolsRecyclerView.setAdapter(adapter);
+      poolsRecyclerView.setLayoutManager(linearLayoutManager);
+
+      return rootView;
+    }
+
+    public void getPoolsJSON() throws IOException {
+      InputStream inputStream = getContext().getAssets()
+              .open("pools_courts.json");
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+      int size;
+      try {
         size = inputStream.read();
+        while (size != -1) {
+          byteArrayOutputStream.write(size);
+          size = inputStream.read();
+        }
+        inputStream.close();
+      } catch (IOException e) {
+        e.printStackTrace();
       }
-      inputStream.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    try {
-      JSONArray jsonArray = new JSONArray(byteArrayOutputStream.toString());
-      String name;
-      String location;
-      String lat;
-      String lon;
+      try {
+        JSONArray jsonArray = new JSONArray(byteArrayOutputStream.toString());
+        String name;
+        String location;
+        String lat;
+        String lon;
 
-      for (int i = 0; i < jsonArray.length(); i++) {
-        name = jsonArray.getJSONObject(i)
-          .getString("Name");
-        location = jsonArray.getJSONObject(i)
-          .getString("Location");
-        lat = jsonArray.getJSONObject(i)
-          .getString("lat");
-        lon = jsonArray.getJSONObject(i)
-          .getString("lon");
+        for (int i = 0; i < jsonArray.length(); i++) {
+          name = jsonArray.getJSONObject(i)
+                  .getString("Name");
+          location = jsonArray.getJSONObject(i)
+                  .getString("Location");
+          lat = jsonArray.getJSONObject(i)
+                  .getString("lat");
+          lon = jsonArray.getJSONObject(i)
+                  .getString("lon");
 
-        ModelPools modelPools = new ModelPools();
-        modelPools.setName(name);
-        modelPools.setLocation(location);
-        modelPools.setLat(lat);
-        modelPools.setLon(lon);
-        modelPools.setImage(R.drawable.pool);
+          ModelPools modelPools = new ModelPools();
+          modelPools.setName(name);
+          modelPools.setLocation(location);
+          modelPools.setLat(lat);
+          modelPools.setLon(lon);
+          modelPools.setImage(R.drawable.pool);
 
 
-        poolsList.add(modelPools);
+          poolsList.add(modelPools);
+        }
+      } catch (JSONException e) {
+        e.printStackTrace();
       }
-    } catch (JSONException e) {
-      e.printStackTrace();
     }
   }
-}
+
