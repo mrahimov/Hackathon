7d80bc2eaf0850054d91a4a3986880fa5e10c19a
Victoria Leon <vcrleon@gmail.com>
2018-03-05 04:09:49 -0500

    finished markers for Google Maps

--- app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/MapFragment.java
+++ app/src/main/java/com/example/murodjonrahimov/hackathon/fragments/MapFragment.java
@@ -2,16 +2,21 @@ package com.example.murodjonrahimov.hackathon.fragments;
 
 
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 import com.example.murodjonrahimov.hackathon.R;
+import com.example.murodjonrahimov.hackathon.backend.AppDatabase;
+import com.example.murodjonrahimov.hackathon.model.MyFavourite;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,9 +25,13 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -48,6 +57,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mapView.getMapAsync(this);
         fLocationPC = LocationServices.getFusedLocationProviderClient(getActivity());
 
+
         return rootView;
     }
 
@@ -69,7 +79,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             this.myGoogleMap = googleMap;
             googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-            googleMap.addMarker(new MarkerOptions().position(new LatLng(40.730610, -73.935242)));
         }
         MapsInitializer.initialize(getContext());
         myGoogleMap = googleMap;
@@ -86,6 +95,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                         double lat = location.getLatitude();
                         double lng = location.getLongitude();
                         LatLng currentLocation = new LatLng(lat, lng);
+                        CameraPosition cameraPosition = new CameraPosition.Builder().target(currentLocation).zoom(12).build();
+                        myGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
                         myGoogleMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
                         myGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
                         UiSettings uiSettings = myGoogleMap.getUiSettings();
@@ -96,5 +107,30 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 }
             });
         }
+
+        Geocoder geocoder = new Geocoder(getContext());
+        AppDatabase db = AppDatabase.getAppDatabase(getActivity());
+        List<MyFavourite> favoriteFellows = db.myFavDao().getFavorite();
+        List<Address> myFavesAddresses;
+        LatLng faves;
+
+        for (int i = 0; i < favoriteFellows.size(); i++) {
+            try {
+                myFavesAddresses = geocoder.getFromLocationName(favoriteFellows.get(i).getLocation()+favoriteFellows.get(i), favoriteFellows.size());
+                if(myFavesAddresses != null) {
+                    for (int j = 0; j < myFavesAddresses.size(); j++) {
+                        Address location = myFavesAddresses.get(j);
+                        faves = new LatLng(location.getLatitude(), location.getLongitude());
+                        CameraPosition cameraPosition = new CameraPosition.Builder().target(faves).zoom(12).build();
+                        myGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                        myGoogleMap.addMarker(new MarkerOptions().position(faves));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
